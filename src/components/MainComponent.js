9d0bed590ce3f51e85237b7e6ca7402020dc03a7
Alexandra <wilderweinundich@gmail.com>
2021-12-25 17:43:50 +0100

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,26 +4,43 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Routes, Route } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
   render() {
 
+    const HomePage = () => {
+      return(
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+      )
+    }
+
     return (
       <div>
         <Header />
         <Routes>
-            <Route path='/home' element={<Home />} />
+            <Route path='/home' element={HomePage()} />
             <Route path='/menu' element={<Menu dishes={this.state.dishes} />} />
+            <Route path='/contactus' element={<Contact />} />
         </Routes>
         <Footer />
       </div>
