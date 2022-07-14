bef53c64630afa6ebe186ffcc233f33b6859986a
Alexandra <wilderweinundich@gmail.com>
2022-07-14 21:52:00 +0200

    part of assignment (1 and 3)

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
-function RenderLeader({leader}) {
-    return (
-        <div key={leader.id} className="col-12 mt-5">
-        <Media tag="li">
-          <Media left middle>
-              <Media object src={leader.image} alt={leader.name} />
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{leader.name}</Media>
-            <p className="mb-3 mt-3">{leader.designation}</p>
-            <p>{leader.description}</p>
-          </Media>
-        </Media>
-      </div>
-    );
+function RenderLeader({leader, isLoading, errMess}) {
 
+    if (isLoading) {
+        return(
+            <Loading />
+        );
+    }
+    else if (errMess) {
+        return(
+            <h4>{errMess}</h4>
+        );
+    }
+    else 
+        return(
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <div key={leader.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={baseUrl + leader.image} alt={leader.name} />
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{leader.name}</Media>
+                            <p className="mb-3 mt-3">{leader.designation}</p>
+                            <p>{leader.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            </FadeTransform>
+        );
 };
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <div className="col-12 m-1"  key={leader.id}>
-                <RenderLeader leader={leader} />
+                <RenderLeader leader={leader} isLoading={props.leadersLoading} errMess={props.leadersErrMess}/>
             </div>
         );
     });
